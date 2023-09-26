f705d0b07c6c89f142ec4fc4fcab2092018fbbaf
Vineeth-Kolichal <vineethchandran5898@gmail.com>
2023-09-27 01:32:03 +0530

    delete todo completed

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -1,37 +1,37 @@
 import TodoSchema from '../models/todo-model.js'
 import mongoose from 'mongoose';
 
-const db=mongoose.connection
+const db = mongoose.connection
 
 export const getAllTodos = async (req, res) => {
 
     try {
-        const todolist= await db.collection('todos').find({ userId: req.userId }).toArray();
-        res.status(200).json({message:"Success",count:todolist.length,todos:todolist });
-        
+        const todolist = await db.collection('todos').find({ userId: req.userId }).toArray();
+        res.status(200).json({ message: "Success", count: todolist.length, todos: todolist });
+
     } catch (error) {
-        res.status(500).json({message:"Error while geting todos",error:error });
+        res.status(500).json({ message: "Error while geting todos", error: error });
     }
 }
 export const addTodo = async (req, res) => {
     try {
-        if(req.body.title){
-            const isCompleted=false;
-            const userId=req.userId;
-            const {title, description}=req.body;
-            const todoModel=new TodoSchema({
+        if (req.body.title) {
+            const isCompleted = false;
+            const userId = req.userId;
+            const { title, description } = req.body;
+            const todoModel = new TodoSchema({
                 userId,
                 title,
                 description,
                 isCompleted
             });
-        const todo=await todoModel.save();
-        res.status(201).json({message:"Todo successfully added",createdAt:todo.createdAt});
-        }else{
-            res.status(400).json({message:"Title is required"});
+            const todo = await todoModel.save();
+            res.status(201).json({ message: "Todo successfully added", createdAt: todo.createdAt });
+        } else {
+            res.status(400).json({ message: "Title is required" });
         }
     } catch (error) {
-        res.status(500).json({message:"Error while adding todo",error:error});
+        res.status(500).json({ message: "Error while adding todo", error: error });
     }
 
 }
@@ -41,18 +41,11 @@ export const updateStatus = async (req, res) => {
 }
 
 export const deleteTodo = async (req, res) => {
-
     try {
-        db.collection('todos').deleteOne({_id:req.params.id}).then((error,status)=>{
-            if(status){
-                res.status(200).json({message:"Todo deleted"});
-            }
-            if(error){
-                res.status(400).json({message:"Error while deleting todo",error:error});
-            }
-        })
-        
+        const id = req.params.id;
+        await TodoSchema.deleteOne({ _id: id });
+        res.status(200).json({ message: "Todo deleted" });
     } catch (error) {
-        res.status(500).json({message:"Error while adding todo",error:error});
+        res.status(500).json({ message: "Error while deleting todo", error: error });
     }
 }

e9a7282c7dffe902f8e0b5828476f75de01676c9
Vineeth-Kolichal <vineethchandran5898@gmail.com>
2023-09-26 17:46:08 +0530

    generate token while sign in success

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -2,7 +2,6 @@ import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import userSchema from "../models/user-model.js";
 import jsonwebtoken from "jsonwebtoken";
-import dotenv from "dotenv";
 
 const db = mongoose.connection;
 export const signup = async (req, res) => {
@@ -24,18 +23,26 @@ export const signup = async (req, res) => {
 
 export const signin = async (req, res) => {
     try {
-        const user=await db.collection('users').findOne({email:req.body.email});
-        if(user){
-            
-            
-        }else{
-            res.status(404).json({message:"user not found"});
+        const user = await db.collection('users').findOne({ email: req.body.email });
+        if (user) {
+            bcrypt.compare(req.body.password, user.password).then((status) => {
+                if (status) {
+                    jsonwebtoken.sign({ id:user._id }, process.env.SECRET_KEY, (err, token) => {
+                        if (token) {
+                            res.status(200).json({ message: "Successfully logged in", token: token });
+                        } else {
+                            res.status(500).json({ message: "Something error", error: err });
+                        }
+                    });
+                } else {
+                    res.status(404).json({ message: "Username and password not matching" })
+                }
+            });
+        } else {
+            res.status(404).json({ message: "user not found" });
         }
-
-        
     } catch (error) {
-        
+        res.status(500).json({ message: "Something error", error: error });
     }
-
 }
 
